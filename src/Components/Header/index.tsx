3fab358e6d8ee98a2ae14d707e9aede4625887f6
yansb <yansbarreiro@gmail.com>
2021-04-12 20:30:57 -0300

    :iphone: sidebar responsivity

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,10 +1,14 @@
-import {Flex, useBreakpointValue} from '@chakra-ui/react';
+import {Flex, Icon, IconButton, useBreakpointValue} from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../context/SidebarDrawerContext';
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
 export function Header(){
+  const {onOpen} = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -21,7 +25,17 @@ export function Header(){
       px="6"
       align="center"
     >
-     <Logo />
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontsize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        />
+      )}
+      <Logo />
 
       {isWideVersion && <SearchBox />}
 
